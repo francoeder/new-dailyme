983d75005560be6d8985adb875494f8875479b97
Éder Franco <ederhf@ciandt.com>
2020-06-18 10:00:58 -0300

    Removendo componente ParticipantList versão 'deprecated'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ParticipantListComponent } from './participant-list/participant-list.component';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ParticipantListComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
